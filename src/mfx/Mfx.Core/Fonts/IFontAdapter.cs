4c1ad5e017958ac1d3c1aeb8253cdcee688594be
Sunny Chen <qingyang.chen2@revvity.com>
2024-08-12 14:40:07 +0800

    Added some comments.

--- src/mfx/Mfx.Core/Fonts/IFontAdapter.cs
+++ src/mfx/Mfx.Core/Fonts/IFontAdapter.cs
@@ -8,10 +8,25 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Mfx.Core.Fonts
 {
+    /// <summary>
+    /// Represents that the implemented classes are font adapters.
+    /// </summary>
     public interface IFontAdapter
     {
-        Vector2 MeasureString(string text);
-
+        /// <summary>
+        /// Draws a text string at the specified position with the specified color.
+        /// </summary>
+        /// <param name="spriteBatch">The <see cref="SpriteBatch"/> used for drawing.</param>
+        /// <param name="text">The text to be drawn</param>
+        /// <param name="position">The position at where the text should be drawn.</param>
+        /// <param name="color">The text drawing color.</param>
         void DrawString(SpriteBatch spriteBatch, string text, Vector2 position, Color color);
+
+        /// <summary>
+        /// Measures the size of a string text with the current font.
+        /// </summary>
+        /// <param name="text">The text to be measured.</param>
+        /// <returns>A <see cref="Vector2"/> instance which represents the size of the text.</returns>
+        Vector2 MeasureString(string text);
     }
 }
